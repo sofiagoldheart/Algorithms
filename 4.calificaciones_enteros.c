6503f17a13581b17775b532f704ec53778609b46
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-01 12:17:41 -0500

    Create 4.calificaciones_enteros.c

---
+++ 4.calificaciones_enteros.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main() {
+        int x=0;
+        scanf("%d",&x);
+            if (x > 6){
+            printf("Aprobado\n");
+            } else {
+            printf("Reprobado\n");
+            } 
+    }
