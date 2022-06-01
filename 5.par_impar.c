238d79ce2b9dcab6fd02b6df7ac307d0c7f237ac
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-01 12:41:28 -0500

    Create 5.par_impar.c

---
+++ 5.par_impar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main() {
+        int x=0;
+        int r=0;
+        scanf("%d",&x);
+            if (x%2 == 0){
+            printf("Par\n");
+            } else {
+            printf("Impar\n");
+            } 
+    }
