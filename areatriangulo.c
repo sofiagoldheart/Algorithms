bf5c9e67f1d5b566917e12e1fe653a64dab963d8
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-01 11:31:29 -0500

    Create areatriangulo.c

---
+++ areatriangulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  int main() {
+    float b=0;
+    float h=0;
+    float r=0;
+      scanf("%f",&b);
+      scanf("%f",&h);
+        r=b*h/2;
+      printf("El area es: \n%.2f",r);
+    return 0;
+}
