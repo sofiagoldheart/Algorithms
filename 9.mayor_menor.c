0e469a90563efdf47400259fef0aaed4a2a52318
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-02 12:42:51 -0500

    Create 9.mayor_menor.c

---
+++ 9.mayor_menor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n=0;
+    
+    scanf("%d",&n);
+    
+    if (n>10){
+        printf("Es mayor a 10");
+    }
+    else{
+        printf("Es menor a 10");
+    }
+}
