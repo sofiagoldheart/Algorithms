ae58c09ef92a944f4da2193397e540ef66285340
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-02 12:12:29 -0500

    Create 7.if_anidados.c

---
+++ 7.if_anidados.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int x,y,z=0;
+    
+    scanf("%d",&x);
+    scanf("%d",&y);
+    scanf("%d",&z);
+
+        if (x>y){
+            if(x>z){
+            printf("%d",x);
+            }
+            else {
+            printf("%d",z);
+            }
+        } else {
+            if(y>z){
+                printf("%d",y);}
+            else {
+            printf("%d",z);
+            }
+        }
+}
