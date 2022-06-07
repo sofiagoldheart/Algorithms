235e9fe5db1dea2e3385535ae8f8722e5b620d33
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-07 12:11:02 -0500

    Create 10. anidado_cal.c

---
+++ 10. anidado_cal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int grade=0;
+    printf("Insert grade\n");
+    scanf("%d",&grade);
+    
+    if (grade>=0){
+        if (grade<=10){
+            if (grade>6){
+            printf("A");
+                } 
+                else {
+                printf("F");
+                }
+            } else {
+            printf("Invalid");
+            }
+        } else {
+            printf("Invalid");
+        }
+    }    
+    
