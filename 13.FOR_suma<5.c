d4894c7ca34d38b279f708267bfcd3c08be6c508
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-08 12:03:28 -0500

    Create 13.FOR_suma<5.c

---
+++ 13.FOR_suma<5.c
@@ -0,0 +1,13 @@
+/* ciclo FOR: for (inicio; límite; incremento) {}
+ex: for (int i=0; i<=10; i++) {}*/
+#include <stdio.h>
+int main() {
+    int suma=0;
+    int n=0;
+        for (int i=0; i<5; i++){
+            printf("Ingresa 5 números:\n");
+            scanf("%d",&n);
+                suma=suma+n;
+    }
+    printf("%d\n", suma);
+}
