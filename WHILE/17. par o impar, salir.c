1b7fbd8fc976e050966289dae141406a5a02e272
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-09 12:40:09 -0500

    Create 17. par o impar, salir.c

---
+++ WHILE/17. par o impar, salir.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+int n=0;
+int c=0;
+
+  do{
+    printf("Ingresa un valor:\n");
+    scanf("%d",&n);
+    if (n%2==0){
+      printf("Es un número par\n");
+    } else {
+      printf("Es un número impar\n");
+    }
+    printf("Si desea continuar presione 1\n");
+    scanf("%d",&c);
+    }
+    while (c==1);
+    printf("Ha decidido salir");  
+    return 0;
+}
