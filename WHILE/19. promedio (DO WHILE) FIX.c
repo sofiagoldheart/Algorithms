5f5a549e02a0491da0137189113fe5177f937963
Sofia de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-13 13:23:33 -0500

    Update and rename 19. promedio (DO WHILE).c to 19. promedio (DO WHILE) FIX.c

---
+++ WHILE/19. promedio (DO WHILE) FIX.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+float n=0, suma=0, promedio=0;
+int x=0, int i=0;
+
+  do{ 
+    printf("Ingresa una calificación:\n");
+    scanf("%f",&n);
+    
+    if (n>0 || n<=10) {
+    i++
+    suma=suma+n;
+    promedio=suma/i;
+      if (promedio>=7){
+      printf("Aprobada con %f\n", promedio);
+      } else {
+      printf("Reprobada con %.2f\n", promedio);
+      }
+        } else {
+      printf("Calificación inválida\n",);
+      printf("¿Desea continuar? (Presione 1 si desea continuar)\n");
+      scanf("%d",&x);
+    } while (x==1);
+      printf("Ha decidido salir");  
+      return 0;
+  }
