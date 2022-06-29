28efc67ac0c7c9ca6a0e5bdd1542ec99b39313ec
Sofia de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-29 11:57:42 -0500

    Update 32. array string for.c

--- CONTADORES Y ARRAYS/32. array string for.c
+++ CONTADORES Y ARRAYS/32. array string for.c
@@ -12,3 +12,23 @@ int main(){
       printf("%c",name[i]);
     }
 }
+
+
+//cadenacadenacadena
+#include <stdio.h>
+
+int main(){
+    char name[5];
+
+    printf("Ingresa un nombre:\n");
+    scanf("%s",name);
+    printf("Tu nombre es: %s\n",name);
+  
+    for(int i=0; i<=3; i++){
+      printf("%c",name[0]);
+      printf("%c",name[1]);
+      printf("%c",name[2]);
+      printf("%c",name[3]);
+
+    }
+}
