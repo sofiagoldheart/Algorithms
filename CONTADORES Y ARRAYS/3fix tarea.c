55c3fa80ce067701669382a19d3a7a736be7ba42
Sofia de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-06-30 11:27:52 -0500

    Update 3fix tarea.c

--- CONTADORES Y ARRAYS/3fix tarea.c
+++ CONTADORES Y ARRAYS/3fix tarea.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-
-int main(){
-    char cadena1[10];
-    char cadena2[10];
-    int x=0;
-  do{
-   for (int i=0; i<10; i++){
-    printf("Ingresa dos palabras:\n");
-    scanf("%s",cadena1);
-    scanf("%s",cadena2);
-      if (cadena1[i]==cadena2[i]){
-      printf("Ambas palabras son iguales\n");
-      printf("¿Desea continuar? (Presione 1 si desea continuar)\n");
-      scanf("%d",&x);
-    }else{
-        printf("Ambas palabras no son iguales\n");
-        printf("¿Desea continuar? (Presione 1 si desea continuar)\n");
-        scanf("%d",&x);
+#include <string.h>
+ 
+int main()
+{
+    char s1[10],s2[10];  
+    int i,c=0;
+ 
+    printf("Ingresa la primera cadena: ");
+    gets(s1);
+    printf("Ingresa la primera cadena: ");
+    gets(s2);
+    if(strlen(s1)==strlen(s2))
+    {
+    	for(i=0;s2[i]!='\0';i++)  
+        {
+        	if(s1[i]==s2[i])
+        	 c++;
+ 	    }
+ 	    if(c==i)
+          printf("Las cadenas son iguales");
+        else
+          printf("Las cadenas no son iguales");
     }
-    }
-    }
-  while(x==1);
-      printf("Ha decidido salir");  
-      return 0;
-
+    else
+     printf("Las cadenas no son iguales");
+    return 0;
 }
